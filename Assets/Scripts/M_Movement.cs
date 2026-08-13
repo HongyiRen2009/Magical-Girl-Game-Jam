f1d2678bb06bdaf466a6c967a7b7b76f7653e569
AlexH-CO <aj.hunter.co@gmail.com>
2026-08-13 14:29:54 -0600

    Movement mod working
    finished the base functionalitys for movement mod
    
    we can add bezier curves, handles for interactive gizmos, and more options for controlling speed when in a path
    
    we may also want to combine this with the homing script, as that is a component of movement

--- Assets/Scripts/M_Movement.cs
+++ Assets/Scripts/M_Movement.cs
@@ -11,8 +11,10 @@ public class M_Movement : Mod
     [AllowNesting] [HideIf("followPath")] [SerializeField] float acceleration;
 
     [AllowNesting] [ShowIf("followPath")] [SerializeField] Vector3 start;
-    [AllowNesting] [ShowIf("followPath")] [SerializeField] Point[] points;
     [AllowNesting] [ShowIf("followPath")] [SerializeField] bool destroyOnFinish = true;
+
+    [Header("Points")] // show if doesnt work in this case so im just making it a seperate catagory
+    [SerializeField] Point[] points;
 
     [Header("Gizmos")]
     [SerializeField] bool drawGizmos;
@@ -53,6 +55,13 @@ public class M_Movement : Mod
                     }
                 }
             }
+            else
+            {
+                if (destroyOnFinish)
+                {
+                    projectile.Despawn();
+                }
+            }
         }
         else
         {
@@ -80,18 +89,30 @@ public class Point
     public Vector3 position; // the position of the target
     public float time; // the time in seconds that it takes to traverce this path
     public bool orbit; // determaines if the path will be a line from the current position to the target, or if the path will circle around the target
-    
-    [ShowIf("orbit")]
-    public bool clockwise;
 
     [ShowIf("orbit")]
-    public float distance;
+    public float travel;
 
     public void PlaceBulletAlongPath(Projectile projectile, float t, Vector3 previous)
     {
+        float traveled = t/time;
+
         if (!orbit)
         {
-            projectile.transform.position = previous + (position-previous) * (t/time);
+            projectile.transform.position = previous + (position-previous) * traveled;
+        }
+        else
+        {
+            Vector3 vectorAngle = previous - position;
+            float angle = Mathf.Atan2(vectorAngle.y, vectorAngle.x);
+            float radius = vectorAngle.magnitude;
+
+            float gotoAngle = angle + travel/radius;
+
+            float difference = gotoAngle - angle;
+            float currentAngle = angle + difference * traveled;
+
+            projectile.transform.position = new Vector3(Mathf.Cos(currentAngle)*radius, Mathf.Sin(currentAngle)*radius, 0) + position;
         }
         
     }
@@ -114,21 +135,36 @@ public class Point
                 float angle = Mathf.Atan2(vectorAngle.y, vectorAngle.x);
                 float radius = vectorAngle.magnitude;
 
-                float gotoAngle = angle + distance/radius * (clockwise ? -1 : 1);
+                float gotoAngle = angle + travel/radius;
 
                 Vector3 pPosition = new Vector3(Mathf.Cos(angle)*radius, Mathf.Sin(angle)*radius, 0);
                 pPosition += position;
 
-                // for (clockwise ? (float cAngle = angle + 0.1f; cAngle < gotoAngle; cAngle += 0.1f) : (float cAngle = angle + 0.1f; cAngle < gotoAngle; cAngle += 0.1f))
-                // {
-                //     Vector3 cPosition = new Vector3(Mathf.Cos(cAngle)*radius, Mathf.Sin(cAngle)*radius, 0);
-                //     cPosition += position;
+                if (travel > 0)
+                {
+                    for (float cAngle = angle + 0.1f; cAngle < gotoAngle; cAngle += 0.1f)
+                    {
+                        Vector3 cPosition = new Vector3(Mathf.Cos(cAngle)*radius, Mathf.Sin(cAngle)*radius, 0);
+                        cPosition += position;
 
-                //     Gizmos.DrawLine(cPosition, pPosition);
+                        Gizmos.DrawLine(cPosition, pPosition);
 
-                //     pPosition = cPosition;
-                // }
+                        pPosition = cPosition;
+                    }
+                }
+                else
+                {
+                    for (float cAngle = angle - 0.1f; cAngle > gotoAngle; cAngle -= 0.1f)
+                    {
+                        Vector3 cPosition = new Vector3(Mathf.Cos(cAngle)*radius, Mathf.Sin(cAngle)*radius, 0);
+                        cPosition += position;
 
+                        Gizmos.DrawLine(cPosition, pPosition);
+
+                        pPosition = cPosition;
+                    }
+                }
+                
                 Gizmos.DrawLine(pPosition, new Vector3(Mathf.Cos(gotoAngle)*radius, Mathf.Sin(gotoAngle)*radius, 0) + position);
             }
         }
